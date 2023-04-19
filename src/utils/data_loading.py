906e9b74c87a0e177084d5a3a70aa025c526ae31
Korbi <korbinian.federholzner@t-online.de>
2023-04-19 10:35:03 +0000

    moved folder for datasets one level up from src

--- src/utils/data_loading.py
+++ src/utils/data_loading.py
@@ -4,8 +4,8 @@ from torch.utils.data import DataLoader
 
 def get_mnist_dataloaders(batch_size: int = 32) -> Tuple[DataLoader, DataLoader]:
 	transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-	train_dataset = torchvision.datasets.MNIST(root="./", train=True, transform=transform, download=True)
-	test_dataset = torchvision.datasets.MNIST(root="./", train=False, transform=transform, download=True)
+	train_dataset = torchvision.datasets.MNIST(root="./data/", train=True, transform=transform, download=True)
+	test_dataset = torchvision.datasets.MNIST(root="./data/", train=False, transform=transform, download=True)
 
 	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
 	test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
