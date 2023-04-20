8101ac633f7c5affda45f1ae072a743341c6fe49
Korbi <korbinian.federholzner@t-online.de>
2023-04-20 16:45:19 +0000

    added wine quality dataset

--- src/utils/data_loading.py
+++ src/utils/data_loading.py
@@ -1,14 +1,17 @@
 import torch
 import torchvision
 import numpy as np
+import pandas as pd
 from typing import Tuple
 from torch.utils.data import DataLoader
+
+from data.WineQualityDataset import WineQualityDataset
 
 
 def get_mnist_dataloaders(batch_size: int = 32) -> Tuple[DataLoader, DataLoader]:
 	transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-	train_dataset = torchvision.datasets.MNIST(root="./data/", train=True, transform=transform, download=True)
-	test_dataset = torchvision.datasets.MNIST(root="./data/", train=False, transform=transform, download=True)
+	train_dataset = torchvision.datasets.MNIST(root="./datasets/", train=True, transform=transform, download=True)
+	test_dataset = torchvision.datasets.MNIST(root="./datasets/", train=False, transform=transform, download=True)
 
 	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
 	test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
@@ -17,8 +20,8 @@ def get_mnist_dataloaders(batch_size: int = 32) -> Tuple[DataLoader, DataLoader]
 
 def get_mnist_dataset() -> Tuple[torch.Tensor, torch.Tensor]:
 	transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-	train_dataset = torchvision.datasets.MNIST(root="./data/", train=True, transform=transform, download=True).data.float()
-	test_dataset = torchvision.datasets.MNIST(root="./data/", train=False, transform=transform, download=True).data.float()
+	train_dataset = torchvision.datasets.MNIST(root="./datasets/", train=True, transform=transform, download=True).data.float()
+	test_dataset = torchvision.datasets.MNIST(root="./datasets/", train=False, transform=transform, download=True).data.float()
 
 	#torchvision.transforms.Normalize((0.1307,), (0.3081,))
 	train_dataset = torch.nn.functional.normalize(train_dataset)
@@ -28,4 +31,15 @@ def get_mnist_dataset() -> Tuple[torch.Tensor, torch.Tensor]:
 def simple_test_dataset() -> Tuple[torch.Tensor, torch.Tensor]:
 	train = torch.Tensor([1, 0, 1, 0, 1, 0, 1, 0])
 	return train, train
+
+def get_winequality_dataloader(batch_size: int = 32) -> Tuple[DataLoader, DataLoader]:
+	train_dataset = WineQualityDataset("./datasets/WineQuality/winequality-white.csv", train=True)
+	test_dataset = WineQualityDataset("./datasets/WineQuality/winequality-white.csv", test=True)
+
+	train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
+	test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+	return train_loader, test_loader
+
+
 
