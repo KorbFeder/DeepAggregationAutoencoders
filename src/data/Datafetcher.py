8e130ebbafda3707c360d624b6522734eec247e8
Korbi <korbinian.federholzner@t-online.de>
2023-04-24 16:44:35 +0000

    added raise error to abstract class methods

--- src/data/Datafetcher.py
+++ src/data/Datafetcher.py
@@ -4,16 +4,16 @@ from abc import ABC, abstractclassmethod
 class Datafetcher(ABC):
 	@abstractclassmethod
 	def get_train_dataset(self: "Datafetcher") -> Dataset:
-		pass
+		raise NotImplementedError
 
 	@abstractclassmethod
 	def get_test_dataset(self: "Datafetcher") -> Dataset:
-		pass
+		raise NotImplementedError
 
 	@abstractclassmethod
 	def get_train_dataloader(self: "Datafetcher", batch_size: int) -> DataLoader:
-		pass
+		raise NotImplementedError
 
 	@abstractclassmethod
 	def get_test_dataloader(self: "Datafetcher", batch_size: int) -> DataLoader:
-		pass
+		raise NotImplementedError
--- src/fuzzy_logic/Membership.py
+++ src/fuzzy_logic/Membership.py
@@ -4,8 +4,8 @@ from abc import ABC, abstractclassmethod
 class Membership(ABC):
 	@abstractclassmethod
 	def fuzzification(self: "Membership", input_data: torch.Tensor) -> torch.Tensor:
-		pass
+		raise NotImplementedError
 
 	@abstractclassmethod
 	def defuzzification(self: "Membership", input_data: torch.Tensor) -> torch.Tensor:
-		pass
+		raise NotImplementedError
